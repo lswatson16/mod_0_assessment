4595be451161d81a55e1b5b419afe2bf273f1fba
Lauralyn Watson <lauralynw16@gmail.com>
2021-11-12 19:47:05 -0500

    Add an object representing a laptop

---
+++ models/modelA.js
@@ -0,0 +1,11 @@
+var laptop = {
+  name: "Macbook",
+  color: "space gray",
+  inches: 13,
+  memoryGigabyte: 8,
+  retinaDisplay: true,
+  isM1Chip: true,
+  accessories: ["charger", "camera", "keyboard"],
+  applications: ["Slack", "Zoom", "Atom"]
+}
+// console.log(laptop);
