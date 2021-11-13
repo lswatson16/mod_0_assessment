9be2b31f2791dba7b3751607de0c5a19da43df9c
Lauralyn Watson <lauralynw16@gmail.com>
2021-11-12 19:55:51 -0500

    Add a model representing a house

---
+++ models/modelB.js
@@ -0,0 +1,12 @@
+var house = {
+  name: "Casa Blanca",
+  ownerName: "Ava",
+  numParkingGarage: 8,
+  numPools: 7,
+  heatedDriveway: true,
+  hasValet: true,
+  houseStaff: ["gardener", "grounds keeper", "doorman"],
+  rooms: ["conservatory", "billiards", "movie theater"]
+}
+
+// console.log(house);
